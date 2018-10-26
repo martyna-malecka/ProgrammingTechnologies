7c91340433555aa6374df916d19f9696ad9bb110
Szymon Rogalski <44255539+superszymi@users.noreply.github.com>
2018-10-26 19:12:22 +0200

    Base version of the program data and logic layers

---
+++ Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRental
+{
+    class Program
+    {
+        private TapeCatalog tapes = new TapeCatalog();
+        private ClientCatalog clients = new ClientCatalog();
+        private RentalCatalog rentals = new RentalCatalog();
+
+
+        void Rent(Tape tape, Client client)
+        {
+            Rental rental = new Rental();
+
+            rentals.Add(rental);
+            tape.SetStatus(false);
+        }
+
+        void Return(Rental rental)
+        {
+            rental.GetTape().SetStatus(true);
+
+            if(DateTime.Today > rental.GetDueDate()) //if overtime
+            {
+                rental.GetClient().SetDebt((DateTime.Today - rental.GetDueDate()).Days);
+                rental.SetStatus(true);
+            }
+            else
+            {
+                rental.SetStatus(true);
+            }
+        }
+
+        void AddTape(String title, String director, String genre, int year)
+        {
+            tapes.Add(new Tape(year, title, director, genre));
+        }
+
+        void AddClient(String name, String surname)
+        {
+            clients.Add(new Client(name, surname));
+        }
+
+        List<Client> GetClientsInDebt()
+        {
+            List<Client> clientsInDebt = new List<Client>();
+            foreach (Client client in clients)
+            {
+                if (client.GetDebt() != 0)
+                {
+                    clientsInDebt.Add(client);
+                }
+            }
+            return clientsInDebt;
+        }
+
+        List<Tape> GetAvailableTapes()
+        {
+            List<Tape> availableTapes = new List<Tape>();
+            foreach(Tape tape in tapes)
+            {
+                if(tape.GetStatus() == true)
+                {
+                    availableTapes.Add(tape);
+                }
+            }
+            return availableTapes;
+        }
+
+        List<Rental> GetCurrentRentals()
+        {
+            List<Rental> currentRentals = new List<Rental>();
+            foreach(Rental rental in rentals)
+            {
+                if(rental.GetStatus() == false)
+                {
+                    currentRentals.Add(rental);
+                }
+            }
+            return currentRentals;
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Rental.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoRental
+{
+    class Rental
+    {
+        private Client client;
+        private Tape rentedTape;
+        private String id;
+        private bool returned;
+        private DateTime rentDate;
+        private DateTime dueDate;
+
+        public Rental()
+        {
+
+        }
+
+        public Rental(Client _client, Tape _rentedTape)
+        {
+            client = _client;
+            rentedTape = _rentedTape;
+            id = Guid.NewGuid().ToString();
+            returned = false;
+            rentDate = DateTime.Today;
+            dueDate = rentDate.AddDays(30);
+        }
+
+        public Client GetClient()
+        {
+            return client;
+        }
+
+        public Tape GetTape()
+        {
+            return rentedTape;
+        }
+
+        public String GetID()
+        {
+            return id;
+        }
+
+        public void SetStatus(bool _returned)
+        {
+            returned = _returned;
+        }
+
+        public bool GetStatus()
+        {
+            return returned;
+        }
+
+        public DateTime GetRentDate()
+        {
+            return rentDate;
+        }
+
+        public DateTime GetDueDate()
+        {
+            return dueDate;
+        }
+
+    }
+}
